e888704c08c580da5b06a2be1eb1d0089422d4cb
dpicattoqumind <daniel.picatto@pluralit.io>
2024-05-28 12:42:59 +0200

    dani_app test

---
+++ test/__init__.py
@@ -0,0 +1,14 @@
+import unittest
+from app import app
+
+class BasicTests(unittest.TestCase):
+    def setUp(self):
+        self.app = app.test_client()
+        self.app.testing = True
+
+    def test_home(self):
+        response = self.app.get('/')
+        self.assertEqual(response.status_code, 200)
+
+if __name__ == '__main__':
+    unittest.main()
